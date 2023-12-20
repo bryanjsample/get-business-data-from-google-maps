1817d3c37171169728222d173e2551fef78f3c99
bryanjsample <bryanjsample@gmail.com>
2023-12-20 15:46:49 -0600

    Update scrape-business-data.py

--- scrape-business-data.py
+++ scrape-business-data.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium 
+from bs4 import BeautifulSoup
+
 
 driver = webdriver.Chrome()
 
 driver.get('https://www.google.com/maps/search/restaurants+in+bemidji')
 
-items = driver.find_elements(by=By.CLASS_NAME, value='hfpxzc')
+items =  driver.find_elements(By.CLASS_NAME, value='hfpxzc')
 
+for i in items:
+    href = i.find_element(By.XPATH, '//href[@href]')
+    print(href.text)
+
+
+    
 
 
 
