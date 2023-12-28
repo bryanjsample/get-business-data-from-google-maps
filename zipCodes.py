571c77f54b2f001d61247d7f994eb7cb5c772062
Bryan Sample <bryanjsample@gmail.com>
2023-12-28 00:33:26 -0700

    preparing for zip codes

--- zipCodes.py
+++ zipCodes.py
@@ -1,1 +1,10 @@
-# this is new
+# user input 
+#   large or small search?
+#   if small, continue with og script
+#   if large, by city or county?
+
+# parse csv
+#   for row in csv, if county == county, append zip into list
+#   for row in csv, if city == city, append zip into list
+
+# maps search = industry + in + zip code list
